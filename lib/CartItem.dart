ba03011998441a61e2bee08e9bf21fe82682b109
trantuankiet123 <trantuankiet11884@gmail.com>
2023-05-19 10:16:26 +0700

    four time

--- lib/CartItem.dart
+++ lib/CartItem.dart
@@ -70,27 +70,11 @@ class CartItem extends StatelessWidget {
                 SizedBox(
                   height: 8.0,
                 ),
-                Row(
-                  children: [
-                    Text(
-                      'Quantity :',
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.remove),
-                      onPressed: () {},
-                    ),
-                    Text(
-                      '$quantity',
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.add),
-                      onPressed: () {},
-                    ),
-                  ],
+                Text(
+                  'Quantity: $quantity',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
                 ),
               ],
             ),
